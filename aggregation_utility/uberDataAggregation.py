6e977e240b49d0bbf2ab64e3c010d40115333e81
Shawn Lee <slee208@ucr.edu>
2017-02-06 21:37:41 +0000

    Added more to main()

--- aggregation_utility/uberDataAggregation.py
+++ aggregation_utility/uberDataAggregation.py
@@ -1,5 +1,5 @@
 # python imports
-import sys, getopt
+import sys, getopt, time, os.path, queue, csv
 
 # uber API library imports
 from uber_rides.session import Session
@@ -9,16 +9,14 @@ from uber_rides.errors import *
 # querying framework library import
 # from AgentQueryFramework import *
 
-# turn Sandbox on or off
-
 # OAuth 2.0 credentials
-# UBER_SERVER_TOKEN = "0pX05LDtY4wn4I5cub9IM-rmfHdmjO45Ma3vPVRo"
-# UBER_CLIENT_ID = "A3nKaRh5hSyq9RprDmJzAiWbc0F7Yu9H"
-# UBER_CLIENT_SECRET = "f-MBa7v-km1Y1qp2-aEurMrup7mXeRSnxeqQ5Pin"
-# REDIRECT_URL = "https://127.0.0.1"
-# UBER_PERMISSION_SCOPES = ["request"]
-# PRIVILEGED = 0
-# NON_PRIVILEGED = 1
+UBER_SERVER_TOKEN = "0pX05LDtY4wn4I5cub9IM-rmfHdmjO45Ma3vPVRo"
+UBER_CLIENT_ID = "A3nKaRh5hSyq9RprDmJzAiWbc0F7Yu9H"
+UBER_CLIENT_SECRET = "f-MBa7v-km1Y1qp2-aEurMrup7mXeRSnxeqQ5Pin"
+REDIRECT_URL = "https://127.0.0.1"
+UBER_PERMISSION_SCOPES = ["request"]
+
+UBER_REQUEST_LIMIT = 2000
 
 
 # def uber_query(location, dist):
@@ -124,22 +122,24 @@ from uber_rides.errors import *
 #     return session
 
 def usage():
-  print 'usage: ' + sys.argv[0] + '[option] ... [-h help | -s boolean | -p boolean | -o file ]'
+  print 'usage: ' + sys.argv[0] + '[option] ... [-h help | -s boolean | -p boolean | -i file | -o file ]'
   print 'Options and arguments (and corresponding environment variables):'
   print '-h \t\t: print this help message and exit (also --help)'
   print '-s boolean \t: sandbox mode; takes a boolean value to enable (True) or disable (False) sandbox mode. Default is False.'
   print '-p boolean \t: privileged mode; takes a boolean value to enable (True) or disable (False) privileged mode. Default is False.'
+  print '-i file \t: changes the program\'s input directory; takes a string for file directory.'
   print '-o file \t: changes the program\'s output directory; takes a string for file directory. Default is None.'
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "h:s:p:o", ["help", "sandbox=", "privileged=", "output="])
+        opts, args = getopt.getopt(sys.argv[1:], "h:s:p:i:o", ["help", "sandbox=", "privileged=", "input=", "output="])
     except getopt.GetoptError as err:
         print(err)
         usage()
         sys.exit(2)
-        
-    output = None
+    
+    src_file = None
+    dst_file = None
     sandbox = False
     privileged = False
     
@@ -152,14 +152,55 @@ def main():
             sandbox = a
         elif o in ("-p", "--privileged"):
             privileged = a
+        elif o in ("-i", "--input"):
+            src_file = a
         elif o in ("-o", "--output"):
-            output = a
+            dst_file = a
         else:
             assert False, "unhandled option"
-
+            
+    if None in (src_file, dst_file):
+        print "Source and destination files unspecified."
+        usage()
+        sys.exit(2)
+        
     # print output
     # print "S: " + str(sandbox)
     # print "P: " + str(privileged)
+    
+    # Set the distance (in mi) of Uber trip for the run
+    trip_distance = 10
+    
+    # Get lat, long, and location identifier from CSV
+    location_data = get_location_data(src_file)
+    
+    # Set CSV output location for results
+    if not os.path.isfile(dst_file):
+        makeCSV()
+    else:
+        CSV = open(dst_file, "a")
+        CSV.write("\n\n")
+        CSV.close()
+        
+    count, run, total_locations, total_hr_req = 0, 0, 0, 0
+    num_locations = len(location_data)
+    num_uber_reqs = 2
+    start_time = time.time()
+    
+    while 1:
+        elapsed_time = time.time() - start_time
+        
+        for location in location_data:
+            if total_hr_req < UBER_REQUEST_LIMIT - 1 and elapsed_time < 3590:
+                
+                # Run routine from Uber and Lyft
+                try:
+                    data = [location, trip_distance, uber_query(location, trip_distance),
+                            lyft_query(location, trip_distance), weather_query(location)]
+                except IndexError:
+                    while False:
+                        data = [location, trip_distance, uber_query(location, trip_distance),
+                                lyft_query(location, trip_distance), weather_query(location)]
 
 if __name__ == "__main__":
     main()
