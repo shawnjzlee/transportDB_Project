f3dfce2beef7cd5009af8a44e4fc7cdee1aba33c
Shawn Lee <slee208@ucr.edu>
2017-02-27 03:40:38 +0000

    adding spark store script

---
+++ pyspark/sparkStore.sh
@@ -0,0 +1,2 @@
+cqlsh -f weather_station.cql spark13
+spark-submit --master spark://spark13.cs.ucr.edu:7077 --packages TargetHolding:pyspark-cassandra:0.3.5,com.databricks:spark-csv_2.11:1.5.0 --conf spark.cassandra.connection.host=spark13 $@
